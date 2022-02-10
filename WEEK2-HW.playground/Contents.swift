75ad23eacd31ebda9909d0acad075370f7594738
frankdulko <79063207+frankdulko@users.noreply.github.com>
2022-02-10 18:26:53 -0500

    week 2 hw

--- WEEK2-HW.playground/Contents.swift
+++ WEEK2-HW.playground/Contents.swift
@@ -1,3 +1,10 @@
-import UIKit
+import Foundation
 
-var greeting = "Hello, playground"
+func load(_ file :String) -> String {
+  let path = Bundle.main.path(forResource: file, ofType: nil)
+  let str = try? String(contentsOfFile: path!, encoding: .utf8)
+  return str!
+}
+
+print(load("darthvader-ascii.txt"))
+
